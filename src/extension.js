41af22871a7b3cc693a1e8d8e3a38457007c8b18
Gulnaz Serikbay <gserikba@andrew.cmu.edu>
2022-07-04 16:48:56 +0600

    added keybindings, commands

--- src/extension.js
+++ src/extension.js
@@ -1,67 +1,104 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-//https://github.com/vrjuliao/sml-vscode-extension
-const smlEnviron = require('./smlEnvironmentManager');
-//import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+const spawn = require("child_process").spawn;
+
+
+let sml;
+const smlOutput = vscode.window.createOutputChannel("SML");
+let allowNextCommand;
+
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
 
+ //adapted from https://github.com/vrjuliao/sml-vscode-extension
+ function start() {
+     allowNextCommand = false;
+     const cli = vscode.workspace.getConfiguration().get("sml-path", "sml");
+ 
+     var cwd = {};
+     if (vscode.workspace.workspaceFolders !== undefined) {
+         var dir = vscode.workspace.workspaceFolders[0].uri.fsPath;
+         cwd = { cwd: dir };
+     } else {
+         console.log("Unable to set working directory");
+     }
+     
+     sml = spawn(cli, [], Object.assign({ shell: true }, cwd));
+     
+     sml.stdin.setEncoding("utf-8");
+     sml.stdout.setEncoding("utf-8");
+     sml.stderr.setEncoding("utf-8");
+     console.log("started");
+     sml.stdin.read(0);
+ 
+     sml.on("error", function (err) {
+         console.log(err);
+         smlOutput.append(err.message);
+     });
+ 
+     sml.stderr.on("data", (data) => {
+         smlOutput.show(false);
+         smlOutput.append(data + `\n`);
+         allowNextCommand = true;
+     });
+ 
+     sml.stdout.on("data", (data) => {
+         smlOutput.show(false);
+         smlOutput.append(data + `\n`);
+     });
+    smlOutput.show(false);
+    
+ }
 
- let getStream = require('get-stream');
 
- let configuration = vscode.workspace.getConfiguration("sml");
+ async function execCode(code) {
+     while (!sml && !allowNextCommand) { ; }
  
- //below is copied code from ocaml extension
- let doOcpIndent = async (document, token, range) => {
-	 let code = document.getText();
-	 let ocpIndentPath = configuration.get<string>('ocpIndentPath');
-	 let cmd = [ocpIndentPath];
-	 if (range) {
-		 cmd.push('--lines');
-		 cmd.push(`${range.start.line + 1}-${range.end.line + 1}`);
-	 }
-	 cmd.push('--numeric');
-	 let cp = await opamSpawn(cmd, {
-		 cwd: Path.dirname(document.fileName)
-	 });
+     if (sml.exitCode === 0 || sml.exitCode)
+         vscode.window.showErrorMessage("SML process died");
+     else {
+         try {
+             allowNextCommand = false;
+             sml.stdin.write(code + ";;;;\r\n");
+         } catch (error) {
+             smlOutput.append(error.message);
+         }
+     }
+     await vscode.commands.executeCommand(
+         "workbench.action.terminal.scrollToBottom"
+     );
+ }
+
+ function restart() {
+    if (sml.exitCode !== 0 && !sml.exitCode) {
+        sml.stdin.end();
+    }
+    sml.kill();
+    start();
+ }
  
-	 token.onCancellationRequested(() => {
-		 cp.disconnect();
-	 });
+//runs the sources.cm file if exists
+ async function runMake(filename="sources.cm") {
+    const editor = vscode.window.activeTextEditor;
+    restart()
+    if (editor) {
+        execCode(`CM.make "${filename}";`);
+    }
+ }
  
-	 cp.stdin.write(code);
-	 cp.stdin.end();
+     
+ function stop() {
+    sml.stdin.end();
+ }
  
-	 let output = await getStream(cp.stdout);
-	 cp.unref();
-	 if (token.isCancellationRequested) return null;
- 
-	 let newIndents = output.trim().split(/\n/g).map((n) => +n);
-	 let oldIndents = code.split(/\n/g).map((line) => /^\s*/.exec(line)[0]);
- 
-	 let edits = [];
-	 let beginLine = range ? range.start.line : 0;
-	 newIndents.forEach((indent, index) => {
-		 let line = beginLine + index;
-		 let oldIndent = oldIndents[line];
-		 let newIndent = ' '.repeat(indent);
-		 if (oldIndent !== newIndent) {
-			 edits.push(vscode.TextEdit.replace(
-				 new vscode.Range(
-					 new vscode.Position(line, 0),
-					 new vscode.Position(line, oldIndent.length)
-				 ),
-				 newIndent)
-			 );
-		 }
-	 });
- 
-	 return edits;
- };
+
 
  //saving basic keywords here
 let smlTypes  = 'bool,int,real,string,char,word,list,ref,exn}'.split(',');
@@ -69,13 +106,28 @@ let smlKeywords = 'andalso,and,as,abstype,eqtype,datatype,do,div,handle,LESS,GRE
 let reservedModule = 'eqtype,functor,include,sharing,sig,signature,struct,structure,where,:>'.split(',');
 
 function activate(context) {
-	//smlEnviron.start();
-	console.log('Congratulations, your extension "SMLunity" is now active!');
-	let disposable = vscode.commands.registerCommand('smunity.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from SMLunity!');
-	});
 
-	context.subscriptions.push(disposable);
+    start();
+
+	console.log('Congratulations, your extension "SMLunity" is now active!');
+
+    let toVsPos = (pos) => {
+        return new vscode.Position(pos.line - 1, pos.col);
+    };
+    let fromVsPos = (pos) => {
+        return { line: pos.line + 1, col: pos.character };
+    };
+    let toVsRange = (start, end) => {
+        return new vscode.Range(toVsPos(start), toVsPos(end));
+    };
+
+	let runmake = vscode.commands.registerCommand('runMake', 
+        () => runMake("sources.cm"));
+	let restartRepl = vscode.commands.registerCommand('restart', 
+        () => restart());
+
+	context.subscriptions.push(runmake);
+	context.subscriptions.push(restartRepl);	
 
 
 	// autocompletion for basic SML keywords
@@ -93,69 +145,17 @@ function activate(context) {
         })
     );
 
-    //below is for autocompletion, copied code from ocaml extension
-	context.subscriptions.push(
-        vscode.languages.registerCompletionItemProvider({language: 'sml'}, {
-            async provideCompletionItems(document, position, token) {
-                let line = document.getText(new vscode.Range(
-                    new vscode.Position(position.line, 0),
-                    position
-                ));
-                let match = /(?:[~?]?[A-Za-z_0-9'`.]+)$/.exec(line);
-                let prefix = match && match[0] || '';
 
-                await session.syncBuffer(document.fileName, document.getText(), token);
-                if (token.isCancellationRequested) return null;
-
-                let [status, result] = await session.request(['complete', 'prefix', prefix, 'at', fromVsPos(position), 'with', 'doc']);
-                if (token.isCancellationRequested) return null;
-
-                if (status !== 'return') return;
-
-                return new vscode.CompletionList(
-                    result.entries.map(({name, kind, desc, info}) => {
-                        let completionItem = new vscode.CompletionItem(name);
-                        let toVsKind = (kind) => {
-                            switch (kind.toLowerCase()) {
-                                case "value": return vscode.CompletionItemKind.Value;
-                                case "variant": return vscode.CompletionItemKind.Enum;
-                                case "constructor": return vscode.CompletionItemKind.Constructor;
-                                case "label": return vscode.CompletionItemKind.Field;
-                                case "module": return vscode.CompletionItemKind.Module;
-                                case "signature": return vscode.CompletionItemKind.Interface;
-                                case "type": return vscode.CompletionItemKind.Class;
-                                case "method": return vscode.CompletionItemKind.Function;
-                                case "#": return vscode.CompletionItemKind.Method;
-                                case "exn": return vscode.CompletionItemKind.Constructor;
-                                case "class": return vscode.CompletionItemKind.Class;
-                            }
-                    };
-
-                    completionItem.kind = toVsKind(kind);
-                    completionItem.detail = desc;
-                    completionItem.documentation = info;
-                    if (prefix.startsWith('#') && name.startsWith(prefix)) {
-                        completionItem.textEdit = new vscode.TextEdit(
-                            new vscode.Range(
-                                new vscode.Position(position.line, position.character - prefix.length),
-                                position
-                            ),
-                            name
-                        );
-                    }
-                    return completionItem;
-                }));
-            }
-        }, '.', '#'));
 
     context.subscriptions.push(
-        vscode.languages.setLanguageConfiguration('sml', {
+        vscode.languages.setLanguageConfiguration({language:'sml'}, {
             indentationRules: {
                 increaseIndentPattern: /^\s*(type|let)\s[^=]*=\s*$|\b(do|fun|struct|sig)\s*$/,
                 decreaseIndentPattern: /\b(end)\s*$/,
             }
         })
     );
+
 
     // learning to do hovers, for now error checking for 'and'(must be 'andalso')
     context.subscriptions.push(
@@ -164,11 +164,32 @@ function activate(context) {
                 const range = document.getWordRangeAtPosition(position);
                 const word = document.getText(range);
     
-                if (word == "and") {
+                if (word == "and" || word == "&" || word == "&&") {
                     return new vscode.Hover({
-                        language: "Hello language",
+                        language: "sml",
                         value: "Maybe 'andalso'?"
                     });
+                }
+                if (word == "or" || word == "||" || word == "|") {
+                    return new vscode.Hover({
+                        language: "sml",
+                        value: "Maybe 'or'?"
+                    });
+                }
+                if (word == "-") {
+                    return new vscode.Hover({
+                        language: "sml",
+                        value: "Maybe '~'?"
+                    });
+                }
+                if (word == "yay"){
+                    return vscode.window.showErrorMessage('Wrong!');
+                }
+                if (word == ":>"){
+                    return new vscode.Hover({
+                        language: "sml",
+                        value: "Opaque signature ascription"
+                    });          
                 }
             }
         })
@@ -180,7 +201,7 @@ function activate(context) {
 
 // this method is called when your extension is deactivated
 function deactivate() {
-	//smlEnviron.stop();
+	stop();
 }
 
 
